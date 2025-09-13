7eb283bef172d8ecedd6ac990bf76a2f602ae3c5
astra-kawa <mjagodzski@gmail.com>
2025-09-13 13:00:08 -0400

    cleanup, resolved warnings

--- src/cdms/xml_parsing.rs
+++ src/cdms/xml_parsing.rs
@@ -1,16 +1,17 @@
-use std::str::FromStr;
-
 use crate::cdms::RawCdmError;
 use crate::cdms::RawConjunctionDataMessage;
 use dioxus::prelude::*;
+#[cfg(any(feature = "server", test))]
 use quick_xml::de::from_str;
 
-#[derive(Debug, serde::Deserialize)]
+#[cfg(any(feature = "server", test))]
+#[derive(serde::Deserialize)]
 struct MessageWrapper {
-    pub cdm: RawConjunctionDataMessage,
+    cdm: RawConjunctionDataMessage,
 }
 
 /// Parse a CDM XML string into a `ConjunctionDataMessage`.
+#[cfg(any(feature = "server", test))]
 pub fn parse_cdm_xml_str(xml: &str) -> Result<RawConjunctionDataMessage, quick_xml::de::DeError> {
     let wrapper: MessageWrapper = from_str(xml)?;
     Ok(wrapper.cdm)
